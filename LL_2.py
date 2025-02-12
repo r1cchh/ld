39a22b8c07d63d037d07c3a2ee43d51350a62c2b
ricards_purmalis_241rdb157 <Ricards.Purmalis@edu.rtu.lv>
2025-02-12 13:21:42 +0200

    Update LL_2.py

--- LL_2.py
+++ LL_2.py
@@ -20,15 +20,20 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
-
-    # WRITE HAS_LOOP METHOD HERE #
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
-    
-    
+        
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+            
+        return False
+            
     
     
 my_linked_list_1 = LinkedList(1)
